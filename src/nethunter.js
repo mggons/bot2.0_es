a6d147d8cce485210bddc205531fb6823a156210
mggons <malagonsxp@hotmail.com>
2021-04-19 11:05:11 -0500

    Update nethunter.js

--- src/nethunter.js
+++ src/nethunter.js
@@ -1,9 +1,9 @@
 const nethunter = () => {
-return `O kali nethunter é um programa que simula o terminal do kali linux no seu termux
-o kali linux é um sistema operacional voltado mais para segurança e invasão de computadores
-com diversos programas e ferramentas de invasão e defesa, uma grande maioria dos hackers utiliza
-esse sistema, o kali nethunter criado pela própia empresa simula essa OS para o terminal android
-segue o link para poder instalar o kali nethunter no seu termux:
+return `Kali nethunter es un programa que simula la terminal kali linux en tu termux
+kali linux es un sistema operativo dirigido más a la seguridad y la intrusión informática
+Con varios programas y herramientas de invasión y defensa, la gran mayoría de los piratas informáticos utilizan
+este sistema, el kali nethunter creado por la propia empresa simula este sistema operativo para el terminal android
+siga el enlace para poder instalar kali nethunter en su termux:
 
 🔰 Kali-Nethunter: https://enrt.eu/UDXILze 🔰`
 
